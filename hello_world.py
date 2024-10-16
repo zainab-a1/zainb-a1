91d09f49bbb98ea7171eb5b001f8cf4e743359da
zainab-a1 <sagalawil@icloud.com>
2024-10-16 02:45:39 +0100

    hello_world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# request user's name
+# print out the name
+# request user's age
+# print out the age
+# print the string "Hello World!."
+
+name = input("Enter your name: ")
+print(name)
+print()
+age = int(input("How old are you: "))
+print(age)
+print()
+print("Hello World!")

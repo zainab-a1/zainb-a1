9c72d9bb414476903101320f6399916e574d8d93
zainab-a1 <sagalawil@icloud.com>
2024-10-16 02:46:50 +0100

    conversion

---
+++ conversion.py
@@ -0,0 +1,14 @@
+# use int to change float to an integer
+# use float to change number to a float
+# use str to change number to string 
+# use int to change from string to whole number
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
